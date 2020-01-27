bec5934da1ce1a8e6da8e475bfaefd630a7555ee
Miguel Fagundez (External) <miguel.fagundez@BCN-ENL-DEV-150.odigeo.org>
2020-01-27 12:44:43 +0100

    Adds Tornado Logging

--- handler/ping_handler.py
+++ handler/ping_handler.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+
 import tornado.web
+from tornado import log
+
 
 class PingHandler(tornado.web.RequestHandler):
     def get(self):
+        log.app_log.info("Receiving ping at {}".format(datetime.now()))
         self.write("pong")
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import logging
+
 import tornado.ioloop
 import tornado.web
 
 from handler.json_ping_handler import JsonPingHandler
 from handler.ping_handler import PingHandler
+from tornado import log
 
 PING_URL = r"/ping"
 JSON_PING_URL = r"/json-ping"
 PORT = 8888
+LOG_LEVEL = logging.INFO
+
 
 def make_app():
     return tornado.web.Application([
@@ -14,7 +19,14 @@ def make_app():
         (JSON_PING_URL, JsonPingHandler)
     ])
 
+
+def _set_log_level():
+    log.app_log.setLevel(LOG_LEVEL) # Logging of errors from application code (i.e. uncaught exceptions from callbacks)
+    log.gen_log.setLevel(LOG_LEVEL) # General-purpose logging, including any errors or warnings from Tornado itself.
+    log.access_log.setLevel(LOG_LEVEL) # Per-request logging for Tornado’s HTTP servers (and potentially other servers in the future)
+
 if __name__ == "__main__":
     app = make_app()
+    _set_log_level()
     app.listen(PORT)
     tornado.ioloop.IOLoop.current().start()
